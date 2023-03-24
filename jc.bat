00fac0fcf0cbbfd6093a2022b4b8f88b5cb39070
Andy Pataki <anpataki@gmail.com>
2023-03-25 00:56:57 +0100

    jc.bat now accepts shorthands as the first argument; the list of shorthands will only render when asked for

--- jc.bat
+++ jc.bat
@@ -2,7 +2,30 @@
 @ECHO OFF
 ::CLS
 
-:MENU
+SET shorthands=42 42r 46 46t 25 25a 25c 52 r qr mp tscp q x
+
+:: %1 is the first argument provided at execution
+IF "%~1"=="" GOTO :INIT
+SET M=%1
+GOTO :DOMATCH
+
+:INIT
+ECHO [90mFor a list of available commands hit [SPACE] then [ENTER].[0m
+:PROMPT
+SET /P M=Type a shorthand then press [ENTER]: 
+:DOMATCH
+FOR %%G IN (%shorthands%) DO (
+            IF /I "%M%"=="%%~G" GOTO :MATCH
+)
+:NOMATCH
+	::CLS
+	ECHO '[91m%M%[0m' is not a known shorthand, please try again
+	GOTO :HELP
+:MATCH
+	ECHO Switching to [96m%M%[0m ...
+	GOTO :%M%
+
+:HELP
 ECHO.
 ECHO [0mÚÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ¿
 ECHO ³[92m        CXA frequently used folders - [7m jc.bat [0m       ³
@@ -14,12 +37,13 @@ ECHO              [96m 25c [97m  025 Client
 ECHO.
 ECHO              [96m 42 [97m   042 Root
 ECHO              [96m 42r [97m  042 React App
-ECHO              [96m 42s [97m  042 Host Suitelet
 ECHO.
 ECHO              [96m 46 [97m   046 Root
 ECHO              [96m 46t [97m  046 Types
 ECHO.
 ECHO              [96m 52 [97m   052 Root
+
+ECHO              [96m qr [97m   quick-rest-api
 ECHO.
 ECHO              [96m r [97m    Run 'npm run reactDev'
 ECHO              [96m mp [97m   Run 'more package.json'
@@ -28,19 +52,7 @@ ECHO.
 ECHO              [96m q/x  [97m EXIT[0m
 ECHO.
 ECHO ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
-
-SET /P M=?? Type a shorthand then press ENTER: 
-SET shorthands=42 42r 42s 46 46t 25 25a 25c 52 r mp tscp q x
-FOR %%G IN (%shorthands%) DO (
-            IF /I "%M%"=="%%~G" GOTO :MATCH
-)
-:NOMATCH
-	::CLS
-	ECHO ?? '[91m%M%[0m' is not a known shorthand, please try again
-	GOTO :MENU
-:MATCH
-	ECHO ?? Switching to [96m%M%[0m ...
-	GOTO :%M%
+GOTO :PROMPT
 
 :25
 cd /d e:\connexa\cxa025\
@@ -57,9 +69,6 @@ GOTO :EXIT
 :42r
 cd /d e:\connexa\cxa042r-sales-manager-react\cxa042r-front-end\cxa042r-react-app\
 GOTO :EXIT
-:42s
-cd /d e:\connexa\cxa042r-sales-manager-react\cxa042r-front-end\cxa042r-suitelet\
-GOTO :EXIT
 :46
 cd /d e:\connexa\cxa046-json-relay\
 GOTO :EXIT
@@ -71,6 +80,9 @@ cd /d e:\connexa\cxa052-work-site-displays\
 GOTO :EXIT
 :r
 npm run reactDev
+GOTO :EXIT
+:qr
+cd /d e:\personal\nodejs-quick-rest-api\
 GOTO :EXIT
 :mp
 more package.json
